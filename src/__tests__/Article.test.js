9772652c4341159bce48b195826256fb23123a57
Edwin kipyego <koriredwin95@gmail.com>
2025-04-10 23:03:56 +0300

    completed lab

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,23 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
+  screen.debug();
+
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
+test("renders a paragraph element", () => {
+  render(<Article />);
+  const paragraphElement = screen.getByText("please pass this test");
+  expect(paragraphElement.tagName).toBe("P");
+});
+
+test("renders the correct text content", () => {
+  render(<Article />);
+  expect(screen.getByText("please pass this test")).toHaveTextContent("please pass this test");
+});
+
+test("renders a div as the parent element", () => {
+  render(<Article />);
+  const parentDiv = screen.getByText("please pass this test").parentElement;
+  expect(parentDiv.tagName).toBe("DIV");
 });
